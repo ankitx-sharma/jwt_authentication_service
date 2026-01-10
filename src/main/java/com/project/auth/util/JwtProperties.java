140a25b60c78c611ae72a206e07dd386c40ea47c
Ankita Sharma <ankitasharma.281993@gmail.com>
2026-01-10 16:53:42 +0100

    Append -> added new file for JWT Properties file

---
+++ src/main/java/com/project/auth/util/JwtProperties.java
@@ -0,0 +1,20 @@
+package com.project.auth.util;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProperties {
+	
+	private String issuer;
+	private String secretBase64;
+	private long accessTokenTtlMinutes;
+	
+	public String getIssuer() { return issuer; }
+	public void setIssuer(String issuer) { this.issuer = issuer; }
+	
+	public String getSecretBase64() { return secretBase64; }
+	public void setSecretBase64(String secretBase64) { this.secretBase64 = secretBase64; }
+	
+	public long getAccessTokenTtlMinutes() { return accessTokenTtlMinutes; }
+	public void setAccessTokenTtlMinutes(long accessTokenTtlMinutes) { this.accessTokenTtlMinutes = accessTokenTtlMinutes; }
+}
